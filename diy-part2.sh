0e042987fb9d38f803630c0bc18bdebd0b09a556
ericbest2022 <wenlong2022@qq.com>
2026-09-13 22:36:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-#============================================================
+#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify luci-app
+git clone https://github.com/theosoft-git/luci-app-easymesh.git package/luci-app-easymesh
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify default IP
 sed -i 's/192.168.*.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-#sed -i 's/OpenWrt/360T7/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/iStoreOS/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/iStoreOS/g' package/base-files/files/etc/init.d/system
+sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/LEDE/iStoreOS/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/iStoreOS/g' package/lean/default-settings/files/zzz-default-settings
 
-# 删除 helloworld / ssr-plus 源码目录
-# rm -rf package/helloworld
-# rm -rf package/lean/luci-app-ssr-plus
+curl -o package/base-files/files/etc/banner https://raw.githubusercontent.com/istoreos/istoreos/refs/heads/istoreos-22.03/package/base-files/files/etc/banner
